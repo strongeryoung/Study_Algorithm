c85268ebcceb6f63acc861d129db08c627e958cb
strongeryoung <141051744+strongeryoung@users.noreply.github.com>
2023-09-27 11:53:35 +0900

    Add files via upload

---
+++ study_week4_class.py
@@ -0,0 +1,65 @@
+
+# 클래스
+
+# 마린 : 공격 유닛, 군인, 총을 쏠 수 있음
+name = "마린" # 유닛의 이름
+hp = 40 # 유닛의 체력
+damage = 5 # 유닛의 공격력
+
+print("{} 유닛이 생성되었습니다.".format(name))
+print("체력 {0}, 공격력 {1}\n".format(hp, damage))
+
+# 탱크 : 공격 유닛, 탱크, 포를 쏠 수 있는데, 일반 모드 / 시즈 모드.
+tank_name = "탱크"
+tank_hp = 150
+tank_damage = 35
+
+print("{0} 유닛이 생성되었습니다.".format(tank_name))
+print("체력 {0}, 공격력 {1}\n".format(tank_hp, tank_damage))
+
+def attack(name, location, damage):
+    print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format( \
+        name, location, damage))
+    
+attack(name, "1시", damage)
+attack(tank_name, "1시", tank_damage)
+
+
+# 탱크 2
+
+tank2_name = "탱크"
+tank2_hp = 150
+tank2_damage = 35
+
+print("{0} 유닛이 생성되었습니다.".format(tank2_name))
+print("체력 {0}, 공격력 {1}\n".format(tank2_hp, tank2_damage))
+
+def attack(name, location, damage):
+    print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format( \
+        name, location, damage))
+    
+attack(name, "1시", damage)
+attack(tank2_name, "1시", tank2_damage)
+
+
+# class
+
+class Unit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print("{0} 유닛이 생성되었습니다.".format(self.name))
+        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+marine1 = Unit("마린", 40, 5)
+marine2 = Unit("마린", 40, 5)
+tank = Unit("탱크", 150, 35)
+marine3 = Unit("마린")  # 오류 - unit self 객체 만큼 입력해줘야 함.
+marine3 = Unit("마린", 40) # 오류 - unit self 객체 만큼 입력해줘야 함.
+
+# 멤버 번수
+
+# 레이스 : 공중 유닛, 비행기, 클로킹(상대방에게 보이지 않음)
+wraith1 = Unit("레이스", 80,)
+348
